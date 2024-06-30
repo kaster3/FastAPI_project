4541379cb6d88ef07c4a58f37b8b5552211725f3
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-06-30 20:40:09 +0200

    create database config

--- fastapi-aplication/core/config.py
+++ fastapi-aplication/core/config.py
@@ -1,10 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
 
 class APIPrefix(BaseModel):
@@ -14,6 +22,7 @@ class APIPrefix(BaseModel):
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     prefix: APIPrefix = APIPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
